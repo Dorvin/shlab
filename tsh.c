773a2abbf756de02179fe49bf87a61d5831a8dee
Dorvin <ladig1017@gmail.com>
2020-04-19 18:09:56 +0900

    add code for eval, builtin_command according to psapp

--- tsh.c
+++ tsh.c
@@ -165,6 +165,34 @@ int main(int argc, char **argv)
 */
 void eval(char *cmdline) 
 {
+    char *argv[MAXARGS];
+    char buf[MAXLINE];
+    int bg;
+    pid_t pid;
+
+    strcpy(buf, cmdline);
+    bg = parseline(buf, argv);
+    if (argv[0] == NULL){
+        return;
+    }
+
+    if(!builtin_cmd(argv)){
+        if((pid = Fork()) == 0){
+            if(execve(argv[0], argv, environ) < 0){
+                printf("%s: Command not found\n", argv[0]);
+                exit(0);
+            }
+        }
+
+        if(!bg){
+            int status;
+            if(waitpid(pid, &status, 0) < 0){
+                unix_error("waitfg: waitpid error");
+            }
+        } else {
+            printf("%d %s\n", pid, cmdline);
+        }
+    }
     return;
 }
 
@@ -231,6 +259,12 @@ int parseline(const char *cmdline, char **argv)
  */
 int builtin_cmd(char **argv) 
 {
+    if(!strcmp(argv[0], "quit")){
+        exit(0);
+    }
+    if(!strcmp(argv[0], "&")){
+        return 1;
+    }
     return 0;     /* not a builtin command */
 }
 
